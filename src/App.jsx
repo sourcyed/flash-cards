4531645248c0669809d634eba76b6d914da08cf1
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-08-15 16:13:52 +0300

    Fixed a database issue by setting the word object id to be the word itself

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
     e.preventDefault()
     if (newWord.trim() === '') return
 
-    const word = { word: newWord, meaning: newMeaning, sentence: newSentence, sentenceMeaning: newSentenceMeaning, picture: newPicture}
+    const word = { id: newWord, word: newWord, meaning: newMeaning, sentence: newSentence, sentenceMeaning: newSentenceMeaning, picture: newPicture}
    
     const duplicate = words.find(w => w.word === newWord)
     

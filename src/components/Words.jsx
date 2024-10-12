46e1911896445e6f6e76df58760e4d785b650781
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-10-12 15:48:52 +0300

    Added progress bar for numbers of words added

--- src/components/Words.jsx
+++ src/components/Words.jsx
@@ -4,19 +4,29 @@ import Word from './Word'
 function Words( {words, highlightHandler, delHandler, swapMeanings} ) {
     const [showMeanings, setShowMeanings] = useState(false)
 
-    let wordNumber = 1
+    const max = Math.ceil(words.length / 1000) * 1000;
+    const value = words.length
+    console.log(max);
+    console.log(value);
+    
+    
     
     return (
-      <>
+      <div style={{border: '1px solid'}}>
         <button onClick={() => setShowMeanings(!showMeanings)}>{!showMeanings ? 'show meanings' : 'hide meanings'}</button>
-        <div style={{ margin: 'auto', height: '50vh', width: '50vh', overflow: 'scroll', border: '1px solid' }}>
+        <div style={{ margin: 'auto', height: '50vh', width: '50vh', overflow: 'scroll' }}>
           <table>
             <tbody>
               {words.map(w => <Word key={w.id} word={w} wordNumber={w.i} onHighlight={() => highlightHandler(w)} showMeanings={showMeanings} swapMeanings={swapMeanings} delHandler={() => delHandler(w)}/>)}
             </tbody>
           </table>
         </div>
-      </>
+        <p>
+          {value} / <strong>{max}</strong>
+          <br />
+          <progress max={max} value={value} style={{ width: '40vh'}}></progress>
+        </p>
+      </div>
     )
   }
 
